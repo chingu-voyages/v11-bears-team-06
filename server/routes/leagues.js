5d92df055ecda4047aa7a465896e46cae35a3a9c
clockwerkz <clockwerkz@gmail.com>
2019-09-08 17:21:31 -0700

    adds fetching for zipCodes basewd on a zip Code and length in miles

--- server/routes/leagues.js
+++ server/routes/leagues.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const fetch = require('isomorphic-unfetch');
+require('dotenv').config();
 
 
 
@@ -7,11 +9,21 @@ const router = express.Router();
 /*  Route:      /leagues
 **  Method:     POST
 **  Status:     Public
-**  Note:       This route will accept a list of zip codes and return all leagues that match a provided zip code.
+**  Note:       This route accepts one zip code and miles, and return all leagues that match a provided zip code.
 */
-router.post('/', (req, res)=> {
-    const { zipCodes } = req.body
-    return res.send({ msg: zipCodes });
+router.post('/', async (req, res)=> {
+    const { zipCode, miles } = req.body
+    if (!zipCode || !miles) return res.status(400).send({msg: 'Missing zip code or miles'});
+    try {
+        const fetchRes = await fetch(`https://www.zipcodeapi.com/rest/${process.env.ZIPCODE_API}/radius.json/${zipCode}/${miles}/mile`,{
+            method: 'GET'
+        });
+        const data = await fetchRes.json();
+        const zipCodes = data.zip_codes.map(addr => addr.zip_code);
+        return res.send(zipCodes);
+    } catch(err) {
+        res.status(400).send({msg: err});
+    }
 })
 
 
