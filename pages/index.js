9368ea3cbfc350070c914a8139cb479057e0faa0
clockwerkz <clockwerkz@gmail.com>
2019-09-09 21:33:29 -0700

    sets up sass as css preprocessor.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import fetch from 'isomorphic-unfetch';
+import './styles.scss';
 
 const Index = (props) => {
     return (
         <div>
             <h1>Bears Team 06</h1>
-            <h2>Voyage 11</h2>
+            <h2 className="example">Voyage 11</h2>
             {props.msg ? <p>This msg is from the backend: {props.msg}</p> : ''}
         </div>
     )
